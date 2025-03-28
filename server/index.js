e0e44b14349ab16df0b7f9feffe1406188673ade
NikhilD1998 <nihkildadab@hotmail.com>
2025-03-28 14:08:35 +0530

    feat: set up server with MongoDB connection, add User model, and update start script

--- server/index.js
+++ server/index.js
@@ -4,6 +4,21 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
+dotenv.config();
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+
+const PORT = 3001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(`${err} did not connect`));
