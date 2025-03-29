c24e8a631023cfbeada0e2e6d9db82a7fe23ecea
NikhilD1998 <nihkildadab@hotmail.com>
2025-03-29 15:07:53 +0530

    feat: implement login page with email and password input fields

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="login">
+      <div className="login_content">
+        <form className="login_content_form">
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => e.target.value}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => e.target.value}
+            required
+          />
+          <button type="submit">Login</button>
+        </form>
+        <a href="/register">Don't have an account? Sign up</a>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
